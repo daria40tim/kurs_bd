914091f39c84309312ab78a2cda8c9f7365768f9
daria40tim <43041800+daria40tim@users.noreply.github.com>
2018-11-13 23:17:42 +0300

    Функционал фиксации ошибок

--- kurs/ViewModel/MainWindowViewModel.cs
+++ kurs/ViewModel/MainWindowViewModel.cs
@@ -12,15 +12,14 @@ using System.Windows;
 namespace kurs.ViewModel
 {
     public class MainWindowViewModel : DependencyObject
-    {
-        int air = 65; int soil = 81; int light = 79; int temp = 100;
+    { 
         public MainWindowViewModel()
         {
+            Collection.FillData();
             Temperature = 16;
             Air_humidity = 65;
             Soil_Moisture = 81;
-            Lightning = 79;
-            Collection.FillData();
+            Lightning = -2;
             DataGridVM = new DataGridViewModel();
             TaskVM = new TaskViewModel();
             SelectedPlant = DataGridVM.SelectedPlant;
@@ -114,8 +113,37 @@ namespace kurs.ViewModel
 
         // Using a DependencyProperty as the backing store for Temperature.  This enables animation, styling, binding, etc...
         public static readonly DependencyProperty TemperatureProperty =
-            DependencyProperty.Register("Temperature", typeof(double), typeof(MainWindowViewModel), new PropertyMetadata(null));
+            DependencyProperty.Register("Temperature", typeof(double), typeof(MainWindowViewModel), new PropertyMetadata(OnTemperatureChanged));
 
+        private static void OnTemperatureChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            foreach (var item in Collection.Plants)
+            {
+                if (item.Cards_of_plant.Where(o => o.Title == "Температура воздуха").FirstOrDefault() != null)
+                {
+                    float optimum = item.Cards_of_plant.Where(o => o.Title == "Температура воздуха").First().Optimal;
+                    float tolerance = item.Cards_of_plant.Where(o => o.Title == "Температура воздуха").First().Tolerance;
+                    float limit = item.Cards_of_plant.Where(o => o.Title == "Температура воздуха").First().Limit_deviation;
+                    float max = optimum + limit;
+                    float min = optimum - limit;
+                    float min_range = optimum - tolerance;
+                    float max_range = optimum + tolerance;
+                    if (int.Parse(d.GetValue(TemperatureProperty).ToString()) <= min || int.Parse(d.GetValue(TemperatureProperty).ToString()) >= max)
+                    {
+                        string message = "Температура воздуха " + d.GetValue(TemperatureProperty).ToString() + " опасна для растений";
+                        string InsString = "insert into errors values (default, @message_str, now(), @card_id)";
+                        using (NpgsqlConnection con = new NpgsqlConnection(ConfigurationManager.ConnectionStrings["connectionString"].ConnectionString))
+                        {
+                            con.Open();
+                            NpgsqlCommand command = new NpgsqlCommand(InsString, con);
+                            command.Parameters.AddWithValue("@message_str", message);
+                            command.Parameters.AddWithValue("@card_id", item.Cards_of_plant.Where(o => o.Title == "Температура воздуха").First().Card_id);
+                            command.ExecuteNonQuery();
+                        }
+                    }
+                }
+            }
+        }
 
         public int Air_humidity
         {
@@ -125,8 +153,37 @@ namespace kurs.ViewModel
 
         // Using a DependencyProperty as the backing store for Air_humidity.  This enables animation, styling, binding, etc...
         public static readonly DependencyProperty Air_humidityProperty =
-            DependencyProperty.Register("Air_humidity", typeof(int), typeof(MainWindowViewModel), new PropertyMetadata(0));
+            DependencyProperty.Register("Air_humidity", typeof(int), typeof(MainWindowViewModel), new PropertyMetadata(OnAirHumidityChanged));
 
+        private static void OnAirHumidityChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            foreach (var item in Collection.Plants)
+            {
+                if (item.Cards_of_plant.Where(o => o.Title == "Влажность воздуха").FirstOrDefault() != null)
+                {
+                    float optimum = item.Cards_of_plant.Where(o => o.Title == "Влажность воздуха").First().Optimal;
+                    float tolerance = item.Cards_of_plant.Where(o => o.Title == "Влажность воздуха").First().Tolerance;
+                    float limit = item.Cards_of_plant.Where(o => o.Title == "Влажность воздуха").First().Limit_deviation;
+                    float max = optimum + limit;
+                    float min = optimum - limit;
+                    float min_range = optimum - tolerance;
+                    float max_range = optimum + tolerance;
+                    if (int.Parse(d.GetValue(Air_humidityProperty).ToString()) <= min || int.Parse(d.GetValue(Air_humidityProperty).ToString()) >= max)
+                    {
+                        string message = "Влажность воздуха " + d.GetValue(Soil_MoistureProperty).ToString() + " опасна для растений";
+                        string InsString = "insert into errors values (default, @message_str, now(), @card_id)";
+                        using (NpgsqlConnection con = new NpgsqlConnection(ConfigurationManager.ConnectionStrings["connectionString"].ConnectionString))
+                        {
+                            con.Open();
+                            NpgsqlCommand command = new NpgsqlCommand(InsString, con);
+                            command.Parameters.AddWithValue("@message_str", message);
+                            command.Parameters.AddWithValue("@card_id", item.Cards_of_plant.Where(o => o.Title == "Влажность воздуха").First().Card_id);
+                            command.ExecuteNonQuery();
+                        }
+                    }
+                }
+            }
+        }
 
         public int Soil_Moisture
         {
@@ -136,8 +193,37 @@ namespace kurs.ViewModel
 
         // Using a DependencyProperty as the backing store for Soil_Moisture.  This enables animation, styling, binding, etc...
         public static readonly DependencyProperty Soil_MoistureProperty =
-            DependencyProperty.Register("Soil_Moisture", typeof(int), typeof(MainWindowViewModel), new PropertyMetadata(0));
+            DependencyProperty.Register("Soil_Moisture", typeof(int), typeof(MainWindowViewModel), new PropertyMetadata(OnSoilMoistureChanged));
 
+        private static void OnSoilMoistureChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            foreach (var item in Collection.Plants)
+            {
+                if (item.Cards_of_plant.Where(o => o.Title == "Влажность почвы").FirstOrDefault() != null)
+                {
+                    float optimum = item.Cards_of_plant.Where(o => o.Title == "Влажность почвы").First().Optimal;
+                    float tolerance = item.Cards_of_plant.Where(o => o.Title == "Влажность почвы").First().Tolerance;
+                    float limit = item.Cards_of_plant.Where(o => o.Title == "Влажность почвы").First().Limit_deviation;
+                    float max = optimum + limit;
+                    float min = optimum - limit;
+                    float min_range = optimum - tolerance;
+                    float max_range = optimum + tolerance;
+                    if (int.Parse(d.GetValue(Soil_MoistureProperty).ToString()) <= min || int.Parse(d.GetValue(Soil_MoistureProperty).ToString()) >= max)
+                    {
+                        string message = "Влажность почвы " + d.GetValue(Soil_MoistureProperty).ToString() + " опасна для растений";
+                        string InsString = "insert into errors values (default, @message_str, now(), @card_id)";
+                        using (NpgsqlConnection con = new NpgsqlConnection(ConfigurationManager.ConnectionStrings["connectionString"].ConnectionString))
+                        {
+                            con.Open();
+                            NpgsqlCommand command = new NpgsqlCommand(InsString, con);
+                            command.Parameters.AddWithValue("@message_str", message);
+                            command.Parameters.AddWithValue("@card_id", item.Cards_of_plant.Where(o => o.Title == "Влажность почвы").First().Card_id);
+                            command.ExecuteNonQuery();
+                        }
+                    }
+                }
+            }
+        }
 
         public int Lightning
         {
@@ -147,9 +233,38 @@ namespace kurs.ViewModel
 
         // Using a DependencyProperty as the backing store for Lightning.  This enables animation, styling, binding, etc...
         public static readonly DependencyProperty LightningProperty =
-            DependencyProperty.Register("Lightning", typeof(int), typeof(MainWindowViewModel), new PropertyMetadata(0));
+            DependencyProperty.Register("Lightning", typeof(int), typeof(MainWindowViewModel), new PropertyMetadata(OnLightningChanged));
 
-
+        private static void OnLightningChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            foreach (var item in Collection.Plants)
+            {
+                if (item.Cards_of_plant.Where(o => o.Title == "Освещенность").FirstOrDefault() != null)
+                {
+                    
+                    float optimum = item.Cards_of_plant.Where(o => o.Title == "Освещенность").First().Optimal;
+                    float tolerance = item.Cards_of_plant.Where(o => o.Title == "Освещенность").First().Tolerance;
+                    float limit = item.Cards_of_plant.Where(o => o.Title == "Освещенность").First().Limit_deviation;
+                    float max = optimum + limit;
+                    float min = optimum - limit;
+                    float min_range = optimum - tolerance;
+                    float max_range = optimum + tolerance;
+                    if (int.Parse(d.GetValue(LightningProperty).ToString()) <= min || int.Parse(d.GetValue(LightningProperty).ToString()) >= max)
+                    {
+                        string message = "Уровень освещения " + d.GetValue(LightningProperty).ToString() + " опасен для растений";
+                        string InsString = "insert into errors values (default, @message_str, now(), @card_id)";
+                        using (NpgsqlConnection con = new NpgsqlConnection(ConfigurationManager.ConnectionStrings["connectionString"].ConnectionString))
+                        {
+                            con.Open();
+                            NpgsqlCommand command = new NpgsqlCommand(InsString, con);
+                            command.Parameters.AddWithValue("@message_str", message);
+                            command.Parameters.AddWithValue("@card_id", item.Cards_of_plant.Where(o => o.Title == "Освещенность").First().Card_id);
+                            command.ExecuteNonQuery();
+                        }
+                    }
+                }
+            }
+        }
 
         public BaseCommand CheckCommand
         {
